79a29eb18cdf93eba69ffa55706a7dd67a7260f8
evgeniia <evgeniia@saunalahti.fi>
2022-06-13 19:46:05 +0300

    added unittests

--- src/tests/test_jps.py
+++ src/tests/test_jps.py
@@ -1,6 +1,6 @@
 import unittest
 from algorithm_core import AlgorithmCore
-from a_star import AStar
+from jps import Jps
 
 class TestAStar(unittest.TestCase):
     def setUp(self):
@@ -9,13 +9,12 @@ class TestAStar(unittest.TestCase):
                     ['@','.','.','.','@'],
                     ['@','.','.','.','@'],
                     ['@','@','@','@','@']]
-        self.test_a_star = AStar(self.data)
-        self.test_core = AlgorithmCore(self.data)
+        self.test_jps = Jps(self.data)
 
-    def test_find_path(self):
+    def test_find_path_available(self):
         start = (1,1)
         end = (3,3)
-        self.assertEqual(self.test_a_star.a_star(start,end)[1], [(1,1),(2,2),(3,3)])
+        self.assertEqual(self.test_jps.jps(start,end)[1], [(1,1),(3,3)])
 
     def test_no_path_available(self):
         test_data = [['@','@','@','@','@'],
@@ -23,12 +22,12 @@ class TestAStar(unittest.TestCase):
                     ['@','.','@','.','@'],
                     ['@','.','@','.','@'],
                     ['@','@','@','@','@']]
-        test_algorithm = AStar(test_data)
+        test_algorithm = Jps(test_data)
         start = (1,1)
         end = (3,3)
-        self.assertEqual(test_algorithm.a_star(start,end), "Polkua ei löytynyt!")
+        self.assertEqual(test_algorithm.jps(start,end), "Polkua ei löytynyt!")
     
     def test_start_same_as_end(self):
         start = (1,1)
         end = (1,1)
-        self.assertEqual(self.test_a_star.a_star(start,end)[1], [(1,1)])
+        self.assertEqual(self.test_jps.jps(start,end)[1], [(1,1)])
